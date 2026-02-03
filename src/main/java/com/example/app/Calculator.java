a4954032f0cacde134dfbfc8481070f8cbdac69e
giovanniiammarino <giovanni.iammarino99@gmail.com>
2026-02-03 15:58:36 +0100

    Setup CI pipeline

--- src/main/java/com/example/app/Calculator.java
+++ src/main/java/com/example/app/Calculator.java
@@ -3,7 +3,7 @@ package com.example.app;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a - b; 
+        return a + b; 
     }
 
     public int multiply(int a, int b) {
